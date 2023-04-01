bcc56b8ec63d427f7320422cbe67d01e75bafbb5
LKosoj <lkosoj@gmail.com>
2023-04-01 15:56:04 +0300

    Update I2CStepper.ino

--- I2CStepper.ino
+++ I2CStepper.ino
@@ -1,5 +1,3 @@
-#define I2CSTEPPER_VERSION 0.1
-
 #define LIQUIDMENU_LIBRARY LiquidCrystal_I2C2_LIBRARY
 #define DisplayClass LiquidCrystal_I2C2
 #define DRIVER_STEP_TIME 5
@@ -7,69 +5,21 @@
 // Подключаем библиотеки:
 #include <Arduino.h>
 #include <Wire.h>                                     // подключаем библиотеку для работы с шиной I2C
+#include <EEPROM.h>
 #include <iarduino_I2C_connect.h>                     // подключаем библиотеку для соединения arduino по шине I2C
 #include <GyverStepper.h>
 #include "PinChangeInterrupt.h"
 #include <GyverEncoder.h>
 #include <TimerOne.h>
 
-// Пины для I2C Master - подключение экрана и потенциально других устройств
-// SDA_PIN 0 //A0
-// SCL_PIN 2 //D2
-
-// Пины для I2C Slave - связь с Samovar
-// SDA_PIN A4
-// SCL_PIN A5
-
-// Пины для шагового двигателя
-#define STEPPER_STEP 3
-#define STEPPER_DIR 4
-#define STEPPER_EN 5
-
-// Настройки для шагового двигателя
-#define STEPPER_MS 2
-#define STEPPER_STEPS (200 * STEPPER_MS) //количество шагов, 200 x MS
-#define STEPPER_MAX_SPEED 1200
-
-
-//Пины для реле
-#define MIXER_PUMP_PIN 13 // RELE_PIN1
-#define RELE_PIN2 10      // RELE_PIN2
-#define RELE_PIN3 11      // RELE_PIN3
-#define RELE_PIN4 12      // RELE_PIN4
-
-// Пины для Encoder
-#define ENC_CLK 7 //S2
-#define ENC_DT 8  //S1
-#define ENC_SW 9  //KEY
-
-/*
-   FREE PIN
-   A1
-   A2
-   A3
-   A6
-   A7
-   D6
-*/
-
-
-GStepper< STEPPER2WIRE> stepper(STEPPER_STEPS, STEPPER_STEP, STEPPER_DIR, STEPPER_EN);
-
-// Объявляем переменные и константы:
-iarduino_I2C_connect I2C2;                            // объект I2C2 для работы c библиотекой iarduino_I2C2_connect
-byte                 REG_Array[9];                    // массив, данные которого будут доступны мастеру (для чтения/записи) по шине I2C
-uint16_t             set_spd;                         // храним значение установленной скорости
-volatile uint16_t    curr_spd;                        // храним предыдущую установленную скорость
-uint16_t             set_time;                        // храним значение установленного времени
-byte                 set_dir;                         // храним значение направления вращения шаговика
-bool                 stepper_state;                   // храним статус шаговика
-byte                 rele_state;                      // байт для статусов 4 реле
-uint8_t              rele_pin[] = {MIXER_PUMP_PIN, RELE_PIN2, RELE_PIN3, RELE_PIN4};//описание пинов реле
-
-Encoder encoder(ENC_CLK, ENC_DT, ENC_SW, TYPE2);
+#include "I2CStepper.h"
 
 #include "I2CMenu.h"
+
+void read_config();
+void write_config();
+
+
 
 void isrENK() {
   encoder.tick();  // отработка в прерывании
@@ -81,7 +31,9 @@ void setup() {
   attachPCINT(digitalPinToPCINT(ENC_DT), isrENK, CHANGE);  //вешаем прерывания для обработки энкодера
   attachPCINT(digitalPinToPCINT(ENC_SW), isrENK, CHANGE);  //вешаем прерывания для обработки энкодера
 
-  Wire.begin(0x01);                                   // инициируем подключение к шине I2C в качестве ведомого (slave) устройства, с указанием своего адреса на шине.
+  read_config();
+
+  Wire.begin(I2CSTPSetup.Type);                                   // инициируем подключение к шине I2C в качестве ведомого (slave) устройства, с указанием своего адреса на шине.
   Wire2.begin();                                      // инициируем подключение к шине I2C в качестве мастера
   I2C2.begin(REG_Array);                              // инициируем возможность чтения/записи данных по шине I2C, из/в указываемый массив
   stepper.setRunMode(FOLLOW_POS);
@@ -327,4 +279,18 @@ void loop() {
       stop_stepper();
     }
   }
+}
+
+void read_config() {
+  EEPROM.get(0, I2CSTPSetup);
+
+  if (isnan(I2CSTPSetup.Type)) {
+    I2CSTPSetup.Type = I2CMIXER;
+    I2CSTPSetup.StepperStepMl = 10000;
+    write_config();
+  }
+}
+
+void write_config() {
+  EEPROM.put(0, I2CSTPSetup);
 }
